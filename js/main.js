34fd66a87a573e7bfd5dbf9e6982f01dd128f33a
kgni <admin@mdia.dk>
2022-03-08 22:26:06 +0100

    browser viewport height fix with JS

--- js/main.js
+++ js/main.js
@@ -18,3 +18,8 @@ postcss([autoprefixer])
 		});
 		console.log(result.css);
 	});
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
