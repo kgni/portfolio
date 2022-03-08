df7b4f20835334a7d37021bd6e03db102c2621e0
kgni <admin@mdia.dk>
2022-03-08 21:45:47 +0100

    add PostCSS autoprefixer

--- js/main.js
+++ js/main.js
@@ -6,3 +6,15 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 		});
 	});
 });
+
+const autoprefixer = require('autoprefixer');
+const postcss = require('postcss');
+
+postcss([autoprefixer])
+	.process(css)
+	.then((result) => {
+		result.warnings().forEach((warn) => {
+			console.warn(warn.toString());
+		});
+		console.log(result.css);
+	});
