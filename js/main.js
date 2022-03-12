20ea892e14df16805a40897df380abf7a29be322
kgni <admin@mdia.dk>
2022-03-12 21:21:41 +0100

    fix viewport on mobile devices JS fix

--- js/main.js
+++ js/main.js
@@ -7,7 +7,12 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 	});
 });
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+// Viewport fix for mobile browsers:
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+	// we get the viewport height and we multiple it by 1% to get a value for a vh unit
+	let vh = window.innerHeight * 0.01;
+	// Then we set the value in the --vh custom property to the root of the document
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
