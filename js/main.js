816badda96c2275c8fd4d12b7d6d5ef54d8cc136
kgni <admin@mdia.dk>
2022-03-12 21:31:57 +0100

    try another vh fix for webkit browsers

--- js/main.js
+++ js/main.js
@@ -7,12 +7,12 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 	});
 });
 
-// Viewport fix for mobile browsers:
+// // Viewport fix for mobile browsers:
 
-// We listen to the resize event
-window.addEventListener('resize', () => {
-	// we get the viewport height and we multiple it by 1% to get a value for a vh unit
-	let vh = window.innerHeight * 0.01;
-	// Then we set the value in the --vh custom property to the root of the document
-	document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+// // We listen to the resize event
+// window.addEventListener('resize', () => {
+// 	// we get the viewport height and we multiple it by 1% to get a value for a vh unit
+// 	let vh = window.innerHeight * 0.01;
+// 	// Then we set the value in the --vh custom property to the root of the document
+// 	document.documentElement.style.setProperty('--vh', `${vh}px`);
+// });
