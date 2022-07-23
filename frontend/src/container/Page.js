14e64ddc21e918ae9ca0b589c5e5472b6a9c6e01
mdiadk <acc@mdia.dk>
2022-07-23 16:17:37 +0200

    fix projects height on smaller devices

--- frontend/src/container/Page.js
+++ frontend/src/container/Page.js
@@ -7,7 +7,7 @@ const Page = ({ children, styles, id }) => {
 				className={`section relative min-h-screen overflow-hidden ${
 					id === 'home'
 						? 'h-screen grow pt-16 pb-24 xs:pb-0'
-						: 'pt-28 pb-24 md:h-auto md:pt-20 md:pb-20'
+						: 'pt-28 pb-24 md:h-auto md:min-h-fit md:pt-20 md:pb-20'
 				} ${styles}`}
 			>
 				<div className="m-auto h-full w-9/12 lg:w-10/12">{children}</div>
